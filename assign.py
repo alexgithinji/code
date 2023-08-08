68ab544e9a5e5c7dd7b4b3b77c99a6757569451d
Alex <alexgithinji335@gmail.com>
2023-08-08 13:35:47 +0300

    assign

---
+++ assign.py
@@ -0,0 +1,18 @@
+"""x=input('enter the 1st number')
+a=int(x)
+#print(type(x))
+y=input('enter the 2nd number')
+b=int(y)
+z=a+b
+print(z)"""
+
+x=int(input('enter the 1st number'))
+y=int(input('enter the 2nd number'))
+z=x+y
+print(z)
+
+
+
+
+
+
