d085e798661db29970ec7ecb9d52b5d6a41a48f9
Alex <alexgithinji335@gmail.com>
2023-08-08 13:13:28 +0300

    while loop

---
+++ if statement.py
@@ -0,0 +1,13 @@
+x=3
+r=x%2
+if r==0:
+    print('even')
+    if x>5:
+     print('great')
+else:
+    print('not great')
+#if(r==1): you can still use this to check whether it is odd
+
+    print('odd')
+    print('Bye')
+
