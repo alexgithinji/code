3dcc6f05d7ff499cdb5d6447c8f7871d436c4c5b
Alex <alexgithinji335@gmail.com>
2023-08-09 19:24:54 +0300

    assign

---
+++ multiple assignment.py
@@ -0,0 +1,53 @@
+#multiple assignment
+
+#name = "alex"
+#age = 21
+#attractive = True
+
+"""name, age, attractive="alex", 21, True
+
+print(name)
+print(age)
+print(attractive)"""
+
+#john=21
+#alex=21
+#roy=21
+
+"""john=alex=roy=21
+
+print(john)
+print(alex)
+print(roy)"""
+
+#string in python
+
+name="alex"
+#print(len(name))
+#print(name.find("l"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("a"))
+#print(name.replace("a","c"))
+#print(name*3)
+
+#type casting in python
+
+x=1    #int
+y=2.0   #float
+z="3"   #str
+
+"""x=int(x)
+y=float(y)
+z=float(z)
+
+print(x)
+print(y)
+print(z*3)"""
+
+print("x is "+str (x))
+print("y is "+str (y))
+print(z*3)
