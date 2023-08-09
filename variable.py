89573a7930124c27a22f9f03f1029be778c65b6d
Alex <alexgithinji335@gmail.com>
2023-08-09 17:56:10 +0300

    assign

---
+++ variable.py
@@ -0,0 +1,29 @@
+first_name = "alex"
+last_name = "githinji"
+full_name = first_name + " " + last_name
+print("Hey " + full_name)
+
+age =21
+age = age + 1
+print(age)
+
+age =21
+age +=1
+print("your age alex is now:",age)
+#print(type(age))
+
+#float data type
+height = 250.5
+print("your height is: "+str(height)+" cm")
+
+#boolean data type
+human =True
+print("are you a human:"+str(human))
+#print(type(human))
+
+
+
+
+
+
+
