c712e80956c5d4258ef5ea1cf2e620f5026ef102
Alex <alexgithinji335@gmail.com>
2023-08-10 12:49:25 +0300

    if and elif statements

---
+++ if statement pro.py
@@ -0,0 +1,13 @@
+age = int(input("How older are you? \n"))
+if age == 100:
+     print("you are a century old!!\n")
+
+elif age >= 18:
+     print("you are an adult! \n")
+
+elif age < 0:
+     print("you haven't been born yet!\n")
+
+else:
+     print("you are a child\n")
+
