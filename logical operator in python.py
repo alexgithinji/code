3f9d7cf36cde4d710ad3ff2218394cc04a7d2d7b
Alex <alexgithinji335@gmail.com>
2023-08-10 13:24:19 +0300

    logical operator

---
+++ logical operator in python.py
@@ -0,0 +1,8 @@
+temp = int(input("what is the temperature outside?: \n"))
+
+if temp >=0 and temp <=30:
+    print("the temperature is good today\n ")
+    print("go outside")
+elif temp < 0 or temp >30:
+    print("the temperature is bad today!")
+    print("stay inside!")
