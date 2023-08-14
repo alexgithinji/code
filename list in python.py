baed740710e64aa1452d77be64d7259c6bdd4978
Alex <alexgithinji335@gmail.com>
2023-08-14 13:39:34 +0300

    list in python

---
+++ list in python.py
@@ -0,0 +1,16 @@
+food =["rice","pizza","hamburger","spaghetti"]
+drinks =["soda","tea","coffee"]
+
+#print(food, "+",drinks)
+food[0]="ugali"
+
+print(food[0])
+food.append("ice scream ")
+food.remove("pizza")
+food.pop()
+#food.sort()
+#food.clear()
+food.insert(0,"cake")
+for x in food:
+    print(x)
+
