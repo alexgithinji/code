b43e95d7f68d3d7d7e15439686ba3c98cd728604
Alex <alexgithinji335@gmail.com>
2023-08-20 23:51:57 +0300

    practice 3

---
+++ web practice.py
@@ -0,0 +1,13 @@
+def main_menu():
+    print("1. welcome to young developers channel")
+    print("2.press yellow button to register as our member")
+    print("3.press white button if you have already registered")
+    print("4. press reb button to exit")
+
+def press_yellow():
+   age = input(int("enter your age"))
+   print("age")
+   choice = input("enter your choice")
+
+   if choice == 1
+    print(input(int("enter your age"))
