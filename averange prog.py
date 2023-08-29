fe7102143f0addaf45a56ab638e8d7c43979e95e
Alex <alexgithinji335@gmail.com>
2023-08-29 23:58:15 +0300

    averange prog

---
+++ averange prog.py
@@ -0,0 +1,10 @@
+# Get input from the user
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+num3 = int(input("Enter the third integer: "))
+
+# Calculate the average
+average = (num1 + num2 + num3) / 3
+
+# Display the result
+print(f"The average of {num1}, {num2}, and {num3} is: {average}")
